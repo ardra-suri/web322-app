f1e06161e85ea7c18beadf3d81140ef8805a32e8
Ardra Surendran <asurendran3@myseneca.ca>
2023-02-22 19:45:29 -0600

    Ass03

--- blog-service.js
+++ blog-service.js
@@ -14,20 +14,6 @@ function readCategories()
           })
       });
 }
-exports.initialize = function(){
-    return new Promise(function(resolve,reject)
-    {
-        fs.readFile("./data/posts.json","utf-8",(err,data)=>{
-            if(err){
-                reject("no results returned")
-            }
-            posts= JSON.parse(data)
-        })
-        resolve("Loaded")
-    })
-    .then(readCategories)
-    .catch((err)=>console.log(err))
-}
             
 exports.getAllPosts = function() {
     return new Promise(function(resolve,reject)
@@ -69,4 +55,105 @@ exports.getCategories = function() {
         }
         resolve(categories)
     })
+}
+
+exports.getPostsByCategory = function(category) {
+    return new Promise(function(resolve,reject)
+    {
+        let tempArr=[]
+        for(let key in posts)
+        {
+            const post = posts[key]
+            if(post.category==category)
+            {
+                tempArr.push(post)
+            }
+        }
+        if(tempArr.length==0)
+        {
+            reject("no results returned")
+        }
+        resolve(tempArr)
+    })
+}
+exports.getPostsByMinDate=function (minDateStr) {
+    return new Promise(function(resolve,reject)
+    {
+        let tempArr=[]
+        for(let key in posts)
+        {
+            const post = posts[key]
+            if(new Date(post.postDate) >= new Date(minDateStr))
+            {
+                tempArr.push(post)
+            }
+        }
+        if(tempArr.length==0)
+        {
+            reject("no results returned")
+        }
+        resolve(tempArr)
+    })
+}
+
+exports.getPostById=function (id) {
+    return new Promise(function(resolve,reject)
+    {
+        let temp={}
+        for(let key in posts)
+        {
+            const post = posts[key]
+            if(post.id==id)
+            {
+                temp=post
+            }
+        }
+        if(Object.keys(temp).length==0)
+        {
+            reject("no results returned")
+        }
+        resolve(temp)
+    })
+}
+exports.addPost=function(postData)
+{
+    return new Promise(function (resolve, reject)
+    {
+        if(postData.published == undefined)
+        {
+            postData.published=false;
+        }
+        else
+        {
+            postData.publised=true;
+        }
+
+        postData.id=posts.length+1;
+        const newval = {
+            id: postData.id,
+            body: postData.body,
+            title: postData.title,
+            category: postData.category,
+            featureImage: postData.featureImage,
+            published: postData.published
+        }
+        console.log(newval)
+        posts.push(newval)
+        resolve(newval)
+      });
+}
+
+exports.initialize = function(){
+    return new Promise(function(resolve,reject)
+    {
+        fs.readFile("./data/posts.json","utf-8",(err,data)=>{
+            if(err){
+                reject("no results returned")
+            }
+            posts= JSON.parse(data)
+        })
+        resolve("Loaded")
+    })
+    .then(readCategories)
+    .catch((err)=>console.log(err))
 }
--- server.js
+++ server.js
@@ -5,9 +5,9 @@
 * 
 *  Name: Ardra Surendran Student ID: 112886213 Date: 2023-02-05
 *
-*  Cyclic Web App URL: ________________________________________________________
+*  Cyclic Web App URL: https://repulsive-pink-adder.cyclic.app/
 *
-*  GitHub Repository URL: ______________________________________________________
+*  GitHub Repository URL: https://github.com/ardra-suri/web322-app
 *
 ********************************************************************************/ 
 
@@ -15,6 +15,18 @@ var server = require("./blog-service");
 var express = require("express");
 var path = require("path");
 var app = express();
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+cloudinary.config({
+    cloud_name: 'drh5zh6nv',
+    api_key: '845322727774918',
+    api_secret: 'Y7UDXe0yaXG7exIutmxphN09jgM',
+    secure: true
+});
+const upload = multer();
+
+
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -40,9 +52,39 @@ app.get("/categories",function(req,res){
 });
 
 app.get("/posts",function(req,res){
-    server.getAllPosts()
+    const category = req.query.category
+    const minDateStr = req.query.minDate
+
+    if(category)
+    {
+server.getPostsByCategory(category)
+.then((data)=>res.send(data))
+        .catch((err)=>res.send(err))
+    }
+    else if(minDateStr)
+    {
+server.getPostsByMinDate(minDateStr)
+.then((data)=>res.send(data))
+        .catch((err)=>res.send(err))
+    }
+    else
+    {
+        server.getAllPosts()
+        .then((data)=>res.send(data))
+        .catch((err)=>res.send(err))
+    }
+    
+});
+
+app.get("/posts/:value",function(req,res){
+    const temp = req.params.value
+    server.getPostById(temp)
     .then((data)=>res.send(data))
     .catch((err)=>res.send(err))
+});
+
+app.get("/posts/add",function(req,res){
+    res.sendFile(path.join(__dirname,"/views/addPost.html"));
 });
 
 app.get("/blog",function(req,res){
@@ -51,7 +93,47 @@ app.get("/blog",function(req,res){
     .catch((err)=>res.send(err))
 });
 
+app.post("/posts/add",upload.single("featureImage"),function(req,res){
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processPost(uploaded.url);
+        });
+    }else{
+        processPost("");
+    }
+     
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+    
+        server.addPost(req.body)
+        .then(res.redirect("/posts"))
+        .catch((err)=>console.log(err))
+    } 
+})
+
 // setup http server to listen on HTTP_PORT
 server.initialize()
 .then(app.listen(HTTP_PORT, onHttpStart()))
-.catch((err)=>console.log(err));
+.catch((err)=>console.log(err))
